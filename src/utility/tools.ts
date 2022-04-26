4834cdc3ac2bc47cec5d9bab19278e44ad624b0c
qi xu <qi.xu@autronicafire.no>
2022-04-26 13:19:29 +0200

    added panel serial number generation

---
+++ src/utility/tools.ts
@@ -0,0 +1,15 @@
+export function makeSN(length: number): string {
+  let result = '';
+  const numbers = '0123456789';
+  const numbersLength = numbers.length;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    if (i === 21) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    } else {
+      result += numbers.charAt(Math.floor(Math.random() * numbersLength));
+    }
+  }
+  return result;
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,19 +15,49 @@
 
     <h3 class="ma-4 primary--text">System :</h3>
 
-    <v-row justify="center">
-      <v-col cols="7">
+    <v-row v-for="(psn, index) in snumbers" :key="index" justify="center">
+      <v-col cols="8">
         <v-text-field
+          readonly
           prepend-icon="mdi-barcode-scan"
-          v-model="snumber"
+          v-model="snumbers[index]"
           label="Serial Number"
         ></v-text-field>
       </v-col>
+    </v-row>
+    <v-row justify="center">
       <v-col cols="1" class="upload-btn" justify="center">
+        <v-btn
+          color="green accent-4"
+          dark
+          class="ml-6"
+          small
+          fab
+          @click="addPSN"
+        >
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col cols="2" class="upload-btn" justify="center">
+        <v-btn
+          color="red darken-1"
+          dark
+          class="ml-6"
+          small
+          fab
+          @click="removePSN"
+        >
+          <v-icon>mdi-minus</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col cols="2" class="upload-btn" justify="center">
+        <v-btn color="cyan" dark @click="generatePSN">generate psn</v-btn>
+      </v-col>
+      <v-col cols="2" class="upload-btn" justify="center">
         <v-btn @click="uploadFile">upload config</v-btn>
       </v-col>
     </v-row>
-    <h3 class="ma-4 primary--text">Features :</h3>
+    <h3 class="mt-8 mr-4 mb-4 ml-4 primary--text">Features :</h3>
     <v-row justify="center">
       <v-col cols="2">
         <v-checkbox v-model="selfVerify" label="Self-Verify"></v-checkbox>
@@ -45,7 +75,11 @@
         <v-btn class="mt-3" color="amber" @click="clearSelection">RESET</v-btn>
       </v-col>
       <v-col cols="1">
-        <v-btn class="mt-3" color="success" :disabled="canSend" @click="send"
+        <v-btn
+          class="mt-3"
+          color="success"
+          :disabled="canSend"
+          @click="getLicensedConfig"
           >Get License-Key</v-btn
         >
       </v-col>
@@ -56,12 +90,13 @@
 <script lang="ts">
 import Vue from 'vue';
 import { uploadASConfig } from '../service/rest';
+import { makeSN } from '../utility/tools';
 
 export default Vue.extend({
   name: 'Home',
   data(): {
     chosenFile: File | null;
-    snumber: string;
+    snumbers: string[];
     selfVerify: boolean;
     coverDetection: boolean;
     analogValue: boolean;
@@ -69,7 +104,7 @@ export default Vue.extend({
   } {
     return {
       chosenFile: null,
-      snumber: '343647193632373121003F00',
+      snumbers: ['343647193632373121003F00'],
       selfVerify: false,
       coverDetection: false,
       analogValue: false,
@@ -84,7 +119,10 @@ export default Vue.extend({
       }
       let formData = new FormData();
       formData.append('config', this.chosenFile);
-      formData.append('system', this.snumber);
+      formData.append('system', this.snumbers.toString());
+      formData.append('sv', this.selfVerify ? 'ture' : 'false');
+      formData.append('cd', this.coverDetection ? 'ture' : 'false');
+      formData.append('av', this.analogValue ? 'ture' : 'false');
 
       uploadASConfig(formData)
         .then((response) => {
@@ -96,12 +134,31 @@ export default Vue.extend({
       this.clearSelection();
     },
 
+    generatePSN() {
+      for (let i = 0; i < this.snumbers.length; i++) {
+        Vue.set(this.snumbers, i, makeSN(24));
+      }
+    },
+
+    addPSN() {
+      this.snumbers = [...this.snumbers, makeSN(24)];
+    },
+
+    removePSN() {
+      if (this.snumbers.length > 1) {
+        this.snumbers.pop();
+      }
+    },
+
+    getLicensedConfig() {
+      console.log('getLicensedConfig');
+    },
     clearSelection() {
       this.chosenFile = null;
       this.selfVerify = false;
       this.coverDetection = false;
       this.analogValue = false;
-      this.snumber = '343647193632373121003F00';
+      this.snumbers = ['343647193632373121003F00'];
       this.canSend = false;
     },
   },
